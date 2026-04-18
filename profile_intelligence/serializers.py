a458c8f5362c8fd1c8a0d2795a48e63273d49e90
Samuel Owoaanu <sam.adebayoadesina@gmail.com>
2026-04-19 00:28:19 +0100

    stable serializer endpoint

--- profile_intelligence/serializers.py
+++ profile_intelligence/serializers.py
@@ -4,9 +4,6 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    # Explicitly define country_probability as optional if it can be null
-    country_probability = serializers.FloatField(required=False, allow_null=True)
-
     class Meta:
         model = Profile
         fields = [
@@ -21,21 +18,20 @@ class ProfileSerializer(serializers.ModelSerializer):
             "country_probability",
             "created_at",
         ]
-        # Make id read-only as it's generated
         read_only_fields = ["id", "created_at"]
 
     def create(self, validated_data):
         new_profile = Profile(
-            name = validated_data["name"],
-            gender = validated_data["gender"],
-            gender_probability = validated_data["gender_probability"],
-            sample_size = validated_data["sample_size"],
-            age = validated_data["age"],
-            age_group = validated_data["age_group"],
-            country_id = validated_data["country_id"],
-            country_probability = validated_data["country_probability"]
+            name=validated_data["name"],
+            gender=validated_data["gender"],
+            gender_probability=validated_data["gender_probability"],
+            sample_size=validated_data["sample_size"],
+            age=validated_data["age"],
+            age_group=validated_data["age_group"],
+            country_id=validated_data["country_id"],
+            country_probability=validated_data["country_probability"],
         )
-        
+
         new_profile.save()
         return new_profile
 
